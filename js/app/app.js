7a384a3e2d78f213ce70f67c040f45c92f00cd54
Daniel Chang <daniel.chang85@gmail.com>
2014-08-05 21:10:14 -0400

    Moar

--- js/app/app.js
+++ js/app/app.js
@@ -19,3 +19,42 @@ Inventory.ItemsItemRoute = Ember.Route.extend({
   }
 });
 
+/** Ember Data **/
+Inventory.Item = DS.Model.extend({
+  name: DS.attr('string'),
+  value: DS.attr('string')
+});
+
+Inventory.Store = DS.Store.extend({
+    adapter: DS.LSAdapter
+});
+
+Inventory.ItemsController = Ember.ArrayController.extend({
+  newItemName: null,
+
+  actions: {
+    createNewItem: function(){
+      var content = this.get('content');
+      console.log("Content: "+content);
+      var newItemName = this.get('newItemName');
+      var unique = newItemName != null && newItemName.length > 1;
+
+      content.forEach(function(item) {
+        if (newItemName === item.get('name')) {
+          unique = false; return;
+        }
+      });
+
+      if (unique) {
+        var newItem = this.store.createRecord('item');
+        newItem.set('id', newItemName);
+        newItem.set('name', newItemName);
+        newItem.save();
+
+        this.set('newItemName', null);
+      } else {
+        alert('Item must have a unique name of at least 2 characters!');
+      }
+    }
+  },
+});
